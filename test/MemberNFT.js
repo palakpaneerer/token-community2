234d3301fe2b1eaab223570b57b8e7d0424cb844
palakpaneerer <dinho.mako@gmail.com>
2023-01-04 11:11:13 +0000

    MemberNFT;テストの前処理実装

--- test/MemberNFT.js
+++ test/MemberNFT.js
@@ -3,20 +3,24 @@ const { ethers } = require("hardhat");
 
 
 describe("MemberNFTコントラクト", function() {
+    let MemberNFT;
+    let memberNFT;
+    const name = "MemberNFT";
+    const symbol ="MEM";
+    let owner;
+
+    beforeEach(async function() {
+        [owner] = await ethers.getSigners();
+        MemberNFT = await ethers.getContractFactory("MemberNFT");
+        memberNFT = await MemberNFT.deploy();
+        await memberNFT.deployed();
+    });
+
     it("トークンの名前とシンボルがセットされるべき",async function() {
-const name = "MemberNFT";
-const symbol = "MEM";
-const MemberNFT = await ethers.getContractFactory("MemberNFT");
-const memberNFT = await MemberNFT.deploy();
-await memberNFT.deployed();
-expect(await memberNFT.name()).to.equal(name);
-expect(await memberNFT.symbol()).to.equal(symbol);
+        expect(await memberNFT.name()).to.equal(name);
+        expect(await memberNFT.symbol()).to.equal(symbol);
     });
     it("デプロイアドレスがオーナーに設定されるべき", async function() {
-        const [owner] = await ethers.getSigners();
-        const MemberNFT = await ethers.getContractFactory("MemberNFT");
-        const memberNFT = await MemberNFT.deploy();
-        await memberNFT.deployed();
         expect(await memberNFT.owner()).to.equal(owner.address);
     });
 });
